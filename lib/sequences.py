8f39812aa64c29cce5e1d48554723925c3b2ffe7
+ <jacqueline.trapp18@gmail.com>
2024-12-10 23:14:47 -0500

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list_i = []
+    for i in range(0, length):
+        if i == 0:
+            element = 0
+        elif i == 1:
+            element = 1
+        else:
+            element = list_i[i-1] + list_i[i-2]
+        
+        list_i.append(element)
+    print(list_i)
